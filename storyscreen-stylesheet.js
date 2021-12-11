9c4fdfa4267530a383b6bdadcb51171700682c6a
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-12-11 12:27:38 +0530

    Create storyscreen-stylesheet.js

---
+++ storyscreen-stylesheet.js
@@ -0,0 +1,101 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  storyContainer: {
+    flex: 1
+  },
+  storyCard: {
+    margin: RFValue(20),
+    backgroundColor: "#2f345d",
+    borderRadius: RFValue(20)
+  },
+  image: {
+    width: "100%",
+    alignSelf: "center",
+    height: RFValue(200),
+    borderTopLeftRadius: RFValue(20),
+    borderTopRightRadius: RFValue(20),
+    resizeMode: "contain"
+  },
+  dataContainer: {
+    flexDirection: "row",
+    padding: RFValue(20)
+  },
+  titleTextContainer: {
+    flex: 0.8
+  },
+  storyTitleText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    color: "white"
+  },
+  storyAuthorText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(18),
+    color: "white"
+  },
+  iconContainer: {
+    flex: 0.2
+  },
+  storyTextContainer: {
+    padding: RFValue(20)
+  },
+  storyText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(15),
+    color: "white"
+  },
+  moralText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(15),
+    color: "white"
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: RFValue(10)
+  },
+  likeButton: {
+    width: RFValue(160),
+    height: RFValue(40),
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: RFValue(30)
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5)
+  }
+});
